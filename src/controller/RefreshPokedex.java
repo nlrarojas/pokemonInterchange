a755a1b3ad1ae007442d87e121b6e53690aec524
nlrarojas <nlrarojas@hotmail.com>
2017-10-03 11:47:17 -0600

    Se implementó la actualización de los cambios respecto al servidor

---
+++ src/controller/RefreshPokedex.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author Nelson
+ */
+public class RefreshPokedex extends Observable{
+    
+    private Object atributeChanged;
+
+    public void setChanged(Object atributeChanged){
+        this.atributeChanged = atributeChanged;
+        this.setChanged();
+        this.notifyObservers(this.atributeChanged);
+    }
+    
+    public Object getAtributeChanged() {
+        return atributeChanged;
+    }
+
+    public void setAtributeChanged(Object atributeChanged) {
+        this.atributeChanged = atributeChanged;
+    }          
+}
